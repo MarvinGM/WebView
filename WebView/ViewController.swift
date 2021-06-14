06d6f08bb341c7befb130f09439020e856271296
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-14 15:20:11 +0800

    Version 1.0

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var webview: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        webview.load(URLRequest(url: URL(string: "https://www.google.com")!))
+
+
     }
-
-
 }
 
